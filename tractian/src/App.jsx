585edbd5a861228ab9f888cba858f7faa353351f
Lara Capila <laracapilak@gmail.com>
2021-10-25 10:18:09 -0300

    Restructure the project's architecture by creating a home component

--- tractian/src/App.jsx
+++ tractian/src/App.jsx
@@ -1,52 +1,15 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Typography } from 'antd';
 
 import './App.css';
-import LogoTractian from './images/LogoTractian.svg';
-import Cards from './components/Cards';
-import SelectAssets from './components/SelectAssets';
+import Details from './pages/Details';
 
-const { Title } = Typography;
-
-const { Header, Content, Footer } = Layout;
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <section>
-      <Layout>
-        <Header style={ { position: 'fixed', zIndex: 1, width: '100%' } }>
-          <div>
-            <img className="logo" src={ LogoTractian } alt="Logo Tractian" />
-          </div>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={ ['2'] }>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Content className="site-layout" style={ { padding: '0 50px', marginTop: 64 } }>
-          <Breadcrumb style={ { margin: '16px 0' } }>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <SelectAssets />
-          <section
-            className="site-layout-background"
-            style={ { padding: 24, minHeight: 380 } }
-          >
-            <Title>Dashboard</Title>
-            <Cards />
-          </section>
-        </Content>
-        <Footer
-          style={ { textAlign: 'center' } }
-        >
-          Created by Lara Capila
-
-        </Footer>
-      </Layout>
-      ,
+      <Home />
+      <Details />
     </section>
   );
 }
