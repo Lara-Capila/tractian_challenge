aa29410a3d66935adaf0d47ce06dcd09bc324d98
Lara Capila <laracapilak@gmail.com>
2021-11-14 20:49:39 -0300

    refactory: remove unused menu in layout

--- tractian/src/pages/Default.jsx
+++ tractian/src/pages/Default.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 
 import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
@@ -23,11 +23,6 @@ export default function Default() {
               <img className="logo" src={ LogoTractian } alt="Logo Tractian" />
             </Link>
           </div>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={ ['2'] }>
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
         </Header>
         <Content className="site-layout" style={ { padding: '0 50px', marginTop: 64 } }>
           <SelectAssets />
