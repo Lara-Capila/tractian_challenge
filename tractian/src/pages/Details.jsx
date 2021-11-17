49e91924ebb6bc17afbc95ee6af2fc7b45b09708
Lara Capila <laracapilak@gmail.com>
2021-11-17 15:39:54 -0300

    style:  Styling back button

--- tractian/src/pages/Details.jsx
+++ tractian/src/pages/Details.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import { Skeleton } from 'antd';
+import { Button, Skeleton } from 'antd';
 
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
@@ -9,6 +9,7 @@ import AssetsContext from '../context/AssetsContext';
 
 export default function Details() {
   const { idAsset } = useContext(AssetsContext);
+  const textButtonBack = 'voltar';
 
   const history = useHistory();
 
@@ -18,7 +19,16 @@ export default function Details() {
 
   return (
     <section>
-      <Link to="/" onClick={ () => goBack }>Voltar</Link>
+      <Button
+        style={ {
+          border: '1px solid #1890ff',
+          marginBottom: 20,
+          borderRadius: 8,
+          letterSpacing: '1px',
+        } }
+      >
+        <Link to="/" onClick={ () => goBack }>{textButtonBack.toUpperCase()}</Link>
+      </Button>
       {idAsset
         ? (
           <AssetDetails />) : <Skeleton active />}
